ded6bdd1229cf06127ac400808ad6258befb7320
DonIgnasio1989 <153773838+DonIgnasio1989@users.noreply.github.com>
2024-02-28 17:14:48 +0000

    zadacha 3

--- Program.cs
+++ Program.cs
@@ -32,7 +32,7 @@ while (true) // Бесконечный цикл
 */
 
 //Задача 2
-using System;
+/*using System;
 class Program
 {
     static void Main()
@@ -55,3 +55,38 @@ class Program
         Console.WriteLine($"\nКоличество четных чисел в массиве: {evenCount}");
     }
 }
+*/
+
+
+//Задача 3
+/*using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] numbers = { 1, 3, 5, 6, 7, 8 }; // Исходный массив
+        int temp;
+        // Вывод исходного массива
+        Console.Write("Исходный массив: ");
+        foreach (int number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+        // Реверсирование массива
+        for (int i = 0; i < numbers.Length / 2; i++)
+        {
+            // Меняем местами элементы
+            temp = numbers[i];
+            numbers[i] = numbers[numbers.Length - 1 - i];
+            numbers[numbers.Length - 1 - i] = temp;
+        }
+        // Вывод измененного массива
+        Console.Write("\nПеревернутый массив: ");
+        foreach (int number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+    }
+}
+*/
+
